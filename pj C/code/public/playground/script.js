84ec599d184f01cf8f32379fe29864057ef32770
yuhangzheng94 <61158372+yuhangzheng94@users.noreply.github.com>
2020-12-03 09:38:40 +0800

    prototyped note taking process

--- pj C/code/public/playground/script.js
+++ pj C/code/public/playground/script.js
@@ -1,25 +1,126 @@
 console.log("hi");
+let noteId = 0;
+
+let textbox = document.getElementById("paper");
 
 let paper = document.getElementById("paper");
+let config = document.getElementById("config-container");
+let btnSubmit = document.getElementById("btnSubmit");
+
+let inputColor = document.getElementById("input-color");
+let inputSize = document.getElementById("input-size");
 
 paper.addEventListener("click", handlePaperClick);
 
+btnSubmit.addEventListener("click", () => {
+  submit();
+});
+
+inputColor.addEventListener("input", () => {
+  textbox.style.color = inputColor.value;
+});
+
+inputSize.addEventListener("input", () => {
+  textbox.style.fontSize = inputSize.value + "px";
+});
+
 function handlePaperClick() {
+  noteId += 1;
   createTextbox();
   paper.removeEventListener("click", handlePaperClick);
   showConfig();
 }
 
 function createTextbox() {
-  let textbox = document.createElement("textarea");
+  textbox = document.createElement("textarea");
+  textbox.id = "textbox" + noteId;
   // textbox is resizable
-  textbox.style.cssText = "background-color: transparent; border: 2px solid; font: Times; padding: 20px; width: 300px; resize: both; overflow: hidden;"
+  textbox.style.cssText = "position: absolute; cursor: move; background-color: transparent; border: 2px solid; padding: 20px; width: 300px; resize: both; overflow: hidden; font-size: 12px"
   // textbox is draggable
 
   // append textbox to paper
   paper.appendChild(textbox);
+  initDrag(textbox);
 }
 
 function showConfig() {
+  // let btnSubmit = document.createElement("button");
+  // btnSubmit.id = "btnSubmit";
+  // btnSubmit.innerHTML = "submit";
+  // btnSubmit.style.cssText = "position: absolute; cursor: pointer; background-color: white; border: 2px solid; padding: 20px; height: 50px; width: 100px";
+  // config.appendChild(btnSubmit);
 
+  config.style.visibility = "visible";
+
+}
+
+function initDrag(el) {
+  // https://www.w3schools.com/howto/howto_js_draggable.asp
+
+  console.log("initDrag");
+  // Make the DIV element draggable:
+  dragElement(el);
+
+  function dragElement(el) {
+    console.log("dragElement");
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(el.id + "header")) {
+      // if present, the header is where you move the DIV from:
+      document.getElementById(el.id + "header").onmousedown = dragMouseDown;
+    } else {
+      // otherwise, move the DIV from anywhere inside the DIV:
+      el.addEventListener("mousedown", dragMouseDown)
+    }
+
+    function dragMouseDown(e) {
+      console.log("dragMouseDown");
+      e = e || window.event;
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      el.addEventListener("mouseup", closeDragElement);
+      el.addEventListener("mouseleave", closeDragElement);
+      // document.onmouseup = closeDragElement;
+
+      // call a function whenever the cursor moves:
+      el.addEventListener("mousemove", elementDrag)
+      // document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+      console.log("elementDrag");
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      el.style.top = (el.offsetTop - pos2) + "px";
+      el.style.left = (el.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+      console.log("closeDragElement");
+      // stop moving when mouse button is released:
+      // el.removeEventListener("mouseup", (e) => {
+      //   closeDragElement(e)
+      // });
+      el.removeEventListener("mousemove", elementDrag);
+      // document.onmouseup = null;
+      // document.onmousemove = null;
+    }
+  }
+}
+
+function submit() {
+  text = document.createElement("p");
+  text.innerHTML = textbox.value;
+  text.style.cssText = "color:" + inputColor.value + "; font-size:" + inputSize.value + "px; position: absolute; top: " + textbox.offsetTop + "px; left:" + textbox.offsetLeft + "px;";
+  paper.appendChild(text);
+  paper.removeChild(textbox);
+  inputColor.value = "#ffffff";
+  inputSize.value = 12;
+  paper.addEventListener("click", handlePaperClick);
 }
