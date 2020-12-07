552cd0b1c861ea158cb4fef5c30f659af4fcca0a
何维翊 <molly@heweiyideMacBook-Pro.local>
2020-12-07 14:28:39 +0800

    try submit, p and textarea default not matching

--- pj C/code/public/script.js
+++ pj C/code/public/script.js
@@ -1,4 +1,4 @@
-// let socket = io();
+let socket = io();
 let p1 = document.getElementById('p1');
 let rollButton = document.getElementById('rollButton');
 let sendButton = document.getElementById('sendButton');
@@ -103,123 +103,132 @@ function closeDragElement() {
 
 
 
-// rollButton.addEventListener("click",()=>{
-//   socket.emit("new-paper");
-// })
-//
-// socket.on("new-paper-to-all",(count)=>{
-//   appendPaper(count);
-// })
-//
-// function appendPaper(i){
-//   let newPaper = document.createElement("img");
-//   newPaper.id = "p"+paperCount;
-//   newPaper.className = "paper";
-//   newPaper.src = "img/folded.png";
-//   paperCount++;
-//   notesgroundWrapper.appendChild(newPaper);
-//
-//   destX = Math.random()*90;
-//   destY = Math.random()*80;
-//   width = 5 + destY/8;
-//
-//   var cssAnimation = document.createElement('style');
-//   cssAnimation.type = 'text/css';
-//   if (paperCount%2 == 0){
-//     var rules = document.createTextNode('@keyframes move'+paperCount+
-//     '{from { left:-10px; } to { left:'+destX+'vw }}');
-//   }else{
-//     var rules = document.createTextNode('@keyframes move'+paperCount+
-//     '{from { right:-10px; } to { right:'+destX+'vw }}');
-//   }
-//   cssAnimation.appendChild(rules);
-//   document.getElementsByTagName("head")[0].appendChild(cssAnimation);
-//
-//   newPaper.style.width = width + "vw";
-//   newPaper.style.top = destY + "vh";
-//   if (paperCount%2 == 0){
-//     newPaper.style.animation = "1s linear 5 spin1, 5s linear 1 forwards move"+paperCount;
-//   }else{
-//     newPaper.style.animation = "1s linear 5 spin2, 5s linear 1 forwards move"+paperCount;
-//   }
-//
-//   newPaper.addEventListener("click",()=>{
-//     notesgroundWrapper.style.visibility="hidden";
-//     rollButton.style.visibility="hidden";
-//     writingPage.style.visibility="visible";
-//     state=newPaper.id;
-//     socket.emit('get-content',state);
-//     console.log("emitted get-content")
-//
-//     sendButton.addEventListener("click",()=>{
-//       // writingPage.style.visibility="hidden";
-//       // notesgroundWrapper.style.visibility="visible";
-//       let message = messagebox.value.trim();
-//       if (message != ""){
-//         let data = {paper: state, color: color.value, font: font.value, size: size.value, message: message};
-//         socket.emit('message-from-one',data);
-//       }
-//       messagebox.value = "";
-//
-//     })
-//
-//     closeButton.addEventListener("click",()=>{
-//       writingPage.style.visibility="hidden";
-//       notesgroundWrapper.style.visibility="visible";
-//       rollButton.style.visibility="visible";
-//       paper.remove();
-//       console.log("removed");
-//       let newDiv = document.createElement("div");
-//       newDiv.id="content";
-//       paperWrapper.prepend(newDiv)
-//     })
-//
-//     console.log("This is "+newPaper.id);
-//   })
-// }
-//
-// socket.on("message-to-all",(data)=>{
-//   console.log(data);
-//   let paper = data.paper;
-//   let color = data.color;
-//   let font = data.font;
-//   let size = data.size;
-//   let message = data.message;
-//   if (state == paper){
-//     appendMessage(color,font,size,message);
-//   }
-// })
-//
-// function appendMessage(color,font,size,message){
-//   let div = document.createElement("div");
-//   let p = document.createElement("p");
-//   p.innerHTML = message;
-//   div.style.color = color;
-//   p.style.fontFamily = font;
-//   div.style.fontSize = size+"px";
-//   div.className = "textDiv";
-//   div.style.left = Math.random()*60+"vh";
-//   div.style.top = Math.random()*70+"vh";
-//   div.appendChild(p);
-//   let paper = document.getElementById('content');
-//   paper.append(div);
-//   console.log("got message")
-// }
-//
-//
-// socket.on('paper-list-data',(count)=>{
-//   if (count!=null){
-//     for (let i=1; i<=count; i++){
-//       appendPaper(i);
-//     }
-//   }
-// })
-//
-// socket.on('archival-data',(dataList)=>{
-//   if (dataList!=null){
-//     for(let i=0; i<dataList.length; i++){
-//       datapoint = dataList[i];
-//       appendMessage(datapoint.color,datapoint.font,datapoint.size,datapoint.message);
-//     }
-//   }
-// })
+rollButton.addEventListener("click",()=>{
+  socket.emit("new-paper");
+})
+
+socket.on("new-paper-to-all",(count)=>{
+  appendPaper(count);
+})
+
+function appendPaper(i){
+  let newPaper = document.createElement("img");
+  newPaper.id = "p"+paperCount;
+  newPaper.className = "paper";
+  newPaper.src = "img/folded.png";
+  paperCount++;
+  notesgroundWrapper.appendChild(newPaper);
+
+  destX = Math.random()*90;
+  destY = Math.random()*80;
+  width = 5 + destY/8;
+
+  var cssAnimation = document.createElement('style');
+  cssAnimation.type = 'text/css';
+  if (paperCount%2 == 0){
+    var rules = document.createTextNode('@keyframes move'+paperCount+
+    '{from { left:-10px; } to { left:'+destX+'vw }}');
+  }else{
+    var rules = document.createTextNode('@keyframes move'+paperCount+
+    '{from { right:-10px; } to { right:'+destX+'vw }}');
+  }
+  cssAnimation.appendChild(rules);
+  document.getElementsByTagName("head")[0].appendChild(cssAnimation);
+
+  newPaper.style.width = width + "vw";
+  newPaper.style.top = destY + "vh";
+  if (paperCount%2 == 0){
+    newPaper.style.animation = "1s linear 5 spin1, 5s linear 1 forwards move"+paperCount;
+  }else{
+    newPaper.style.animation = "1s linear 5 spin2, 5s linear 1 forwards move"+paperCount;
+  }
+
+  newPaper.addEventListener("click",()=>{
+    notesgroundWrapper.style.visibility="hidden";
+    rollButton.style.visibility="hidden";
+    writingPage.style.visibility="visible";
+    state=newPaper.id;
+    socket.emit('get-content',state);
+    console.log("emitted get-content")
+
+    sendButton.addEventListener("click",()=>{
+      // let message = messagebox.value.trim();
+      if (textbox.value != ""){
+        // let pctPosX = ( textbox.offsetLeft - paper.offsetLeft ) / paper.offsetWidth;
+        // let pctPosY = ( textbox.offsetTop - paper.offsetTop ) / paper.offsetHeight;
+        let data = {paper: state, width:textbox.clientWidth, height:textbox.clientHeight, positionX: textbox.offsetLeft, positionY: textbox.offsetTop, color: color.value, font: font.value, size: size.value, message: textbox.value};
+        socket.emit('message-from-one',data);
+      }
+      //CLEAR TEXTAREA HERE!!!
+      // messagebox.value = "";
+
+    })
+
+    closeButton.addEventListener("click",()=>{
+      writingPage.style.visibility="hidden";
+      notesgroundWrapper.style.visibility="visible";
+      rollButton.style.visibility="visible";
+      paper.remove();
+      console.log("removed");
+      let newDiv = document.createElement("div");
+      newDiv.id="content";
+      paperWrapper.prepend(newDiv)
+    })
+
+    console.log("This is "+newPaper.id);
+  })
+}
+
+socket.on("message-to-all",(data)=>{
+  console.log(data);
+  let paper = data.paper;
+  let width = data.width;
+  let height = data.height;
+  let positionX = data.positionX;
+  let positionY = data.positionY;
+  let color = data.color;
+  let font = data.font;
+  let size = data.size;
+  let message = data.message;
+  if (state == paper){
+    appendMessage(width,height,positionX,positionY,color,font,size,message);
+  }
+})
+
+function appendMessage(width,height,positionX,positionY,color,font,size,message){
+  //CHANGE INTO TEXTAREA HERE!
+  let div = document.createElement("div");
+  let p = document.createElement("p");
+  p.innerHTML = message;
+  p.style.width = width + "px";
+  p.style.height = height + "px";
+  div.style.left = positionX + "px";
+  div.style.top = positionY + "px";
+  div.style.color = color;
+  p.style.fontFamily = font;
+  div.style.fontSize = size+"px";
+  // p.className = "textP";
+  div.className = "textDiv";
+  div.appendChild(p);
+  let paper = document.getElementById('content');
+  paper.append(div);
+  console.log("got message")
+}
+
+
+socket.on('paper-list-data',(count)=>{
+  if (count!=null){
+    for (let i=1; i<=count; i++){
+      appendPaper(i);
+    }
+  }
+})
+
+socket.on('archival-data',(dataList)=>{
+  if (dataList!=null){
+    for(let i=0; i<dataList.length; i++){
+      datapoint = dataList[i];
+      appendMessage(datapoint.width,datapoint.height,datapoint.positionX,datapoint.positionY,datapoint.color,datapoint.font,datapoint.size,datapoint.message);
+    }
+  }
+})
